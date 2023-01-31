9cf546e53bb20499f2246fbab14358688d9c63c1
HadiAnik <hadiuzzamananik@gmail.com>
2023-01-31 19:35:50 +0600

    instance-statics-query-methods

--- Lesson-27-Mongoose-Instance-Static-Query/routeHandler/todoHandler.js
+++ Lesson-27-Mongoose-Instance-Static-Query/routeHandler/todoHandler.js
@@ -10,7 +10,7 @@ todoRouter.get("/", async (req, res) => {
   try {
     const data = await Todo.find(filter)
       .select({ _id: 0, __v: 0, date: 0 })
-      .limit(2);
+      .limit();
     res.status(200).json({
       result: data,
       message: "Successfully find todo data.",
@@ -57,6 +57,89 @@ todoRouter.get("/", async (req, res) => {
     //   );
   } catch (error) {
     return res.status(500).send({ message: error });
+  }
+});
+
+// Get active todos
+todoRouter.get("/active", async (req, res) => {
+  const todo = new Todo();
+  try {
+    const data = await todo.findActive();
+    res.status(200).json({
+      result: data,
+      message: "Active status find Successfully.",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "There was a server side error!",
+    });
+  }
+});
+
+// Get active Todos with callback
+todoRouter.get("/active-callback", (req, res) => {
+  const todo = new Todo();
+  todo.findActiveCallback((err, data) => {
+    if (err) {
+      res.status(500).json({
+        message: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        result: data,
+        message: "Active status find Successfully.",
+      });
+    }
+  });
+});
+
+// Get static method todo
+todoRouter.get("/js", async (req, res) => {
+  try {
+    const data = await Todo.findByName("js").select({
+      _id: 0,
+      __v: 0,
+      date: 0,
+    });
+    res.status(200).json({
+      message: "Static search find Successfully.",
+      result: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "There was a server side error!",
+      error,
+    });
+  }
+  // .then((data) => {
+  //   console.log("data", data);
+  //   res.status(200).json({
+  //     result: data,
+  //     message: "Js title find Successfully.",
+  //   });
+  // })
+  // .catch((err) => {
+  //   res.status(500).json({
+  //     message: "There was a server side error!",
+  //   });
+  // });
+});
+
+// Get Query helper method todo       (query method chain hisabe babhor hoy)
+todoRouter.get("/language", async (req, res) => {
+  try {
+    const data = await Todo.find()
+      .byLanguage("react")
+      .select({ _id: 0, __v: 0, date: 0 });
+    res.status(200).json({
+      message: "Static search find Successfully.",
+      result: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "There was a server side error!",
+      error,
+    });
   }
 });
 
--- Lesson-27-Mongoose-Instance-Static-Query/schemas/todoSchema.js
+++ Lesson-27-Mongoose-Instance-Static-Query/schemas/todoSchema.js
@@ -17,4 +17,29 @@ const todoSchema = mongoose.Schema({
   },
 });
 
+// instance methods
+todoSchema.methods = {
+  findActive: function () {
+    return mongoose.model("Todo").find({ status: "active" });
+  },
+  findActiveCallback: (cb) => {
+    return mongoose.model("Todo").find({ status: "active" }, cb);
+  },
+};
+
+// static methods    (do not use arrow function on static bcz not work arrow function)
+// Do not declare statics using ES6 arrow functions (=>). Arrow functions explicitly prevent binding this, so the above examples will not work because of the value of this.
+todoSchema.statics = {
+  findByName: function (name) {
+    return this.find({ title: new RegExp(name, "i") }); // new RegExp(name, "i")  or /js/i
+  },
+};
+
+// query helpers
+todoSchema.query = {
+  byLanguage: function (language) {
+    return this.find({ title: new RegExp(language, "i") });
+  },
+};
+
 module.exports = todoSchema;
